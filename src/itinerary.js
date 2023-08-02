fc48cc3296e352263bd89fe05d6154892cb9c278
misskatiemarple <misskatiemarple@gmail.com>
2023-08-02 16:33:32 +0900

    Added August itinerary details

--- src/itinerary.js
+++ src/itinerary.js
@@ -4,7 +4,11 @@ import "./itinerary.css";
 export default function Itinerary() {
   return (
     <div className="itinerary-container">
-      <h2>hello picture and weather app</h2>
+      <img
+        src="/itinerary/08.2023.jpg"
+        alt="August 2023 itinerary"
+        className="img-fluid itinerary-img"
+      />
     </div>
   );
 }
